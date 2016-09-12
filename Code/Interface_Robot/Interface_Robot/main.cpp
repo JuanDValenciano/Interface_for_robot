eac679a7e4be7624caf431a5cd103ff8bf7c1c64
Juan D Valenciano <juan-da3@hotmail.com>
2016-09-12 13:04:19 -0500

    Add

---
+++ Code/Interface_Robot/Interface_Robot/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Interface_Robot.cpp
+ *
+ * Created: 9/12/2016 12:30:42 PM
+ * Author : Juan_David
+ */ 
+#define F_CPU 16000000UL // frecuencia de reloj
+#include <inttypes.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
